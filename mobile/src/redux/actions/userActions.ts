951706e94b5e01f2ac917a0544df8b2f824d92c7
ocmath <ocmath777@gmail.com>
2023-09-18 22:42:04 -0300

    add: user Location action

--- mobile/src/redux/actions/userActions.ts
+++ mobile/src/redux/actions/userActions.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Dispatch } from "react";
 // import { BASE_URL } from "../../utils";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Address, FoodModel } from "../models";
 
 export interface UpdateLocationAction {
@@ -34,20 +34,20 @@ export type UserAction =
 
 export const onUpdateLocation = (location: Address) => {
     return async (dispatch: Dispatch<UserAction>) => {
-        // try {
-        //     const locationString = JSON.stringify(location);
-        //     await AsyncStorage.setItem("user_location", locationString);
-        //     // save our location in local storage
-        //     dispatch({
-        //         type: "ON_UPDATE_LOCATION",
-        //         payload: location,
-        //     });
-        // } catch (error) {
-        //     dispatch({
-        //         type: "ON_USER_ERROR",
-        //         payload: error,
-        //     });
-        // }
+        try {
+            const locationString = JSON.stringify(location);
+            // await AsyncStorage.setItem("user_location", locationString);
+            // save our location in local storage
+            dispatch({
+                type: "ON_UPDATE_LOCATION",
+                payload: location,
+            });
+        } catch (error) {
+            dispatch({
+                type: "ON_USER_ERROR",
+                payload: error,
+            });
+        }
     };
 };
 
