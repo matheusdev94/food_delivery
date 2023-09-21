425fdf746e477f03c89f79c6efd289a1d904b30f
ocmath <ocmath777@gmail.com>
2023-09-21 14:42:18 -0300

    add: swagger

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import { routes } from "./routes";
-import { initializeDbConnection } from "./db"; //TO DO!!!!
+import { initializeDbConnection } from "./db";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./utils/swaggerConfig";
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 //import routes:
 routes.forEach((route) => {
   app[route.method](route.path, route.handler);
