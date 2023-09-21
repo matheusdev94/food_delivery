da5bbcb736f2232d86a304fe3f7cb8ff764b6b29
ocmath <ocmath777@gmail.com>
2023-09-21 14:43:48 -0300

    add signUp route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,13 +1,3 @@
-import { live } from "./live";
-
-export const routes = [
-  live, //
-];
-// import express from "express";
-
-// const router = express.Router();
-// export const routes = {
-//     router.get('/hello', (req, res) => {
-//         res.
-//     })
-// };
+import { live } from "./liveRoute";
+import { signUp } from "./singUpRoute";
+export const routes = [live, signUp];
